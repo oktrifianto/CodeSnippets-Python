94bcc555833dd6ca2e871d73d718ceb2a86f178b
Rahmat <oktrifianto@gmail.com>
2022-03-13 08:59:55 +0700

    Add `for loop` in a python string

---
+++ for-loops_python/1--for-loop-string.py
@@ -0,0 +1,18 @@
+"""
+########################
+# For loop in a string # 
+########################
+"""
+MyString = "coffee"
+for i in MyString:
+  print(i)
+
+# result:
+"""
+c
+o
+f
+f
+e
+e
+"""
