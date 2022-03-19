5da79eea86125a2cbc7c9ec2c5bba6173d45c878
Rahmat <oktrifianto@gmail.com>
2022-03-19 23:26:14 +0700

    `break` statement in for loop

---
+++ for-loops_python/3--for-loop-break.py
@@ -0,0 +1,16 @@
+###################
+# Break Statement #
+###################
+drink = ["coffee", "tea", "milk"]
+for x in drink:
+  print(x)
+  if x == "tea":
+    break
+
+"""
+------
+result:
+------
+coffee
+tea
+"""
