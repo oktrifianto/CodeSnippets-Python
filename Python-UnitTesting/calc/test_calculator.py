1285b45b66e97279fddaecb3c611ebb12cb04456
Rahmat <oktrifianto@gmail.com>
2022-07-05 12:51:24 +0700

    Add more test cases

--- Python-UnitTesting/calc/test_calculator.py
+++ Python-UnitTesting/calc/test_calculator.py
@@ -13,5 +13,21 @@ class CalcTest(unittest.TestCase):
     self.assertEqual(calculator.add(-1, 10), 9)
     self.assertEqual(calculator.add(2, 4), 6)
 
+  def test_substract(self):   
+    self.assertEqual(calculator.substract(10, 7), 3)
+    self.assertEqual(calculator.substract(-1, -1), 0)
+    self.assertEqual(calculator.substract(2, 4), -2)
+
+  def test_multiply(self):   
+    self.assertEqual(calculator.multiply(10, 7), 70)
+    self.assertEqual(calculator.multiply(-1, 10), -10)
+    self.assertEqual(calculator.multiply(2, 4), 8)
+
+  def test_division(self):   
+    self.assertEqual(calculator.division(10, 2), 5)
+    self.assertEqual(calculator.division(4, 2), 2)
+    self.assertEqual(calculator.division(5, 2), 2.5)
+    self.assertRaises(ValueError, calculator.division, 10, 0) 
+
 if __name__ == '__main__':
   unittest.main()
