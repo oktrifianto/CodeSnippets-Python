2ef8a07cb89a923461153adb88ee92fa0d9439d6
Rahmat <oktrifianto@gmail.com>
2022-07-05 09:54:21 +0700

    Add new file `calculator.py` and methods

---
+++ Python-UnitTesting/calc/calculator.py
@@ -0,0 +1,17 @@
+def add(x, y):
+  return x + y
+
+def substract(x, y):
+  return x - y
+
+def multiply(x, y):
+  return x * y
+
+def division(x, y):
+  if (y <= 0):
+    return 'Can not divided by 0'
+  return x / y
+
+## manual testing
+# print(division(10, 2))
+# print(division(2, 0))
